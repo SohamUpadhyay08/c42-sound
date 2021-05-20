594647757a11dae897ec60b75543ba88f1b429bd
SohamUpadhyay08 <77781974+SohamUpadhyay08@users.noreply.github.com>
2021-05-20 13:38:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var canvas, backgroundImage;
+
+var gameState = 0,finishedPlayers;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var track, car1_img, car2_img, car3_img, car4_img;
+var oil
+var slideSound
+var spill
+var group
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  oil = loadImage("images/f1.png")
+  slideSound = loadSound("sound/sliding.mp3")
+  
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth , displayHeight);
+  database = firebase.database();
+  gameState = 0;
+  distance = 0;
+ // finishedPlayers = 0;
+  yVel = 0;
+  xVel = 0;
+
+  xSet = false;
+  game = new Game();
+  game.getState();
+  game.start();
+  group = createGroup()
+  for(i=0;i<5;i++){
+    w = random(200,950)
+    h = random(-displayHeight*4,displayHeight-300)
+    spill = createSprite(w,h);
+    spill.addImage(oil);
+    group.add(spill)
+  }
+}
+
+
+function draw(){
+   //start the game
+   background(200, 200, 255);
+
+   //start the game
+   if (playerCount === 4 ) {
+     game.update(1);
+   }
+ 
+   //start the game for real
+   if (gameState === 1) {
+     game.play();
+   }
+   if (gameState === 2) {
+     console.log("End");
+   }}
+ 
+  
